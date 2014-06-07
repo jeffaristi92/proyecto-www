f03c5b0723d74291683f8c17015a7d1df1d23ac7
jeffaristi92 <jeff.aristi@gmail.com>
2014-06-06 21:38:33 -0500

    DaoPedido
    Solo metodo insertar

---
+++ Dao/DaoPedido.php
@@ -0,0 +1,30 @@
+<?php
+
+	require_once ('../DataBase/DataBase.php');
+	require_once ('../Logico/Pedido.php');
+	
+	class DaoPedido {
+		private $conexionBd;
+
+		public function __construct(){		
+			$this->conexionBd = new DataBase();
+		}
+		
+		public function insertarPedido($pedido){			
+			
+			$conexion = $this->conexionBd->conectar();
+
+			if ($stmt = $conexion->prepare("INSERT INTO `Pedido`(`fecha`,`estado`,`tipoPago`,`idCajero`) VALUES (?,?,?,?)")){
+	        
+		        $stmt->bind_param('ssss',$pedido->getFecha(),$pedido->getEstado(),$pedido->getTipoPago(),$pedido->getIdCajero());  
+		        $stmt->execute();   
+		        $stmt->store_result();
+				echo "*Pedido registrado con éxito";//mensaje para mostrar al usuario
+	        	
+	        }//Fin consulta
+
+			$this->conexionBd->desconectar($conexion);			
+		}
+		
+	}
+?>
