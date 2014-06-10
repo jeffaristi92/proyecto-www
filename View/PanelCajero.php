a34d825d6bf5ee8dae60e1499a90e11146c05f1d
jhonandres21 <jhonandresmedina@gmail.com>
2014-06-10 00:50:23 -0500

    Estilos Panel Cajero...

--- View/PanelCajero.php
+++ View/PanelCajero.php
@@ -10,69 +10,78 @@
 	}	
 ?>
 
-
 <!DOCTYPE html>
 
 <html lang="en"><head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta charset="utf-8">    
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">    
 
-    <title>Registro Pedido</title>
+    <title>Cajero</title>
 
     <link href="../css/bootstrap.min.css" rel="stylesheet">    
-    <link href="../css/signin.css" rel="stylesheet">
-    <link href="../css/styles.css" rel="stylesheet">
-    <script type="text/javascript" src="../js/jquery-2.1.0.min.js"></script>
-    <script type="text/javascript" src="../js/scripts.js"></script>
+    <link href="../css/stylesAdminSistema.css" rel="stylesheet">  
+    <link href="../css/stylesCajero.css" rel="stylesheet">  
+    <script type="text/javascript" src="../js/jquery-2.1.0.min.js"></script>    
     <script type="text/javascript" src="../js/scriptsPedido.js"></script>
   </head>
 
   <body> 	
+
+    <div class="container contenedor_menu">    
+      <div class="menu">
+         <h1>Bienvenido <?php echo $_SESSION['usuario']?>!</h1>
+         <ul class="pull-right">
+          <a id="cerrar_sesion" href="CerrarSesion.php">Cerrar Sesión</a>  
+         </ul>       
+      </div>
+    </div> <!--FIN Container-->
+
 	<script> var total=0; </script>
-    <a id="cerrar_sesion" href="CerrarSesion.php">Cerrar Sesión</a>	
-    <div class="container">
-        <form class="form-signin" role="form" action="../Controlador/ControllerPedido.php" method="GET">
-        <table>
-        <h2 class="form-signin-heading">Registro Pedido</h2>        
-        <input name="fecha" type="date" class="form-control" required autofocus>
-        <select name="estado" class="form-control" required autofocus>
-            <option value="En Proceso">En Proceso</option>
-            <option value="Realizado">Realizado</option>
-            <option value="Cancelado">Cancelado</option>
-        </select>
-        <select name="tipoPago" class="form-control" required autofocus>
-            <option value="Credito">Credito</option>
-            <option value="Debito">Debito</option>
-            <option value="Efectivo">Efectivo</option>
-        </select>
-        <!--<input name="idCajero" type="string" class="form-control" placeholder="idCajero" required autofocus>-->
-        </table>
-        <table>
-        <select id="platos">
-              <?php require '../Controlador/getPlato.php';?>
-        </select>
-        <input name="cantidadPlato" type="text" id="cantidadPlato" tabindex="14"/>
-        <input type="button" value="Agregar plato" onClick="agregarPlato()"  tabindex="15"/>
-        <select id="listaPlatos" name="listaPlatos[]" multiple="multiple">
-        </select>
-        </table>
-        <table>
-        <select id="adicionales">
-              <?php require '../Controlador/getAdicional.php';?>
-        </select>
-        <input name="cantidadAdicional" type="text" id="cantidadAdicional" tabindex="14"/>
-        <input type="button" value="Agregar Adicional" onClick="agregarAdicional()"  tabindex="15"/>
-        <select id="listaAdicionales" name="listaAdicionales[]" multiple="multiple">
-        </select>
-        </table>
-		
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Registrar</button>
-      </form>
-      <table id="precioTotal"></table>
-		
-    </div> 
+    
+    <div class="container contenedor_contenido">
+      <div class="row">
+        <div class="col-md-6 plato">
+          <div class="wrapper">
+            <h3>Registrar Pedido</h3>  
+
+               <form class="form-signin" role="form" action="../Controlador/ControllerPedido.php" method="GET">
+                    
+                    <input name="fecha" type="date" class="form-control" required autofocus>
+                    <select name="estado" class="form-control" required autofocus>
+                        <option value="En Proceso">En Proceso</option>
+                        <option value="Realizado">Realizado</option>
+                        <option value="Cancelado">Cancelado</option>
+                    </select>
+                    <select name="tipoPago" class="form-control" required autofocus>
+                        <option value="Credito">Credito</option>
+                        <option value="Debito">Debito</option>
+                        <option value="Efectivo">Efectivo</option>
+                    </select>
+                                        
+                    <select id="platos">
+                          <?php require '../Controlador/getPlato.php';?>
+                    </select>
+                    <input name="cantidadPlato" type="text" id="cantidadPlato"/>
+                    <input id="boton" type="button" value="Agregar plato" onClick="agregarPlato()"/>
+                    <select id="listaPlatos" name="listaPlatos[]" multiple="multiple">
+                    </select>
+                    
+                    <select id="adicionales">
+                          <?php require '../Controlador/getAdicional.php';?>
+                    </select>
+                    <input name="cantidadAdicional" type="text" id="cantidadAdicional"/>
+                    <input id="boton" type="button" value="Agregar Adicional" onClick="agregarAdicional()"/>
+                    <select id="listaAdicionales" name="listaAdicionales[]" multiple="multiple">
+                    </select>                    
+
+                    <a class="btn enviar registrar_pedido">Registrar</a>
+                    <button class="btn btn-lg btn-primary btn-block" type="submit">Registrar</button>
+                </form>
+                <table id="precioTotal"></table>
+          </div>
+        </div><!--plato-->
+      </div><!--FIN row-->
+    </div><!--FIN Container-->
+
   </body>
 </html>
